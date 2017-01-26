7ee34bc121e1df460e60fb4445c120ad8173c936
Raphael LEPILLER <raphael.lepiller@gmail.com>
2017-01-26 10:48:44 +0100

    add sort functions

--- heuristiques.c
+++ heuristiques.c
@@ -1,6 +1,100 @@
 # include "heuristiques.h"
 
-void sortRandom(instance *inst);
+int * sortRandom(instance *inst){
+    int nb = inst->nbObjet;
+    int * sortedList = malloc (sizeof(int)*nb);
+    for( int i=0;i<nb;i++){
+        sortedList[i]=i;
+    }
+    if (nb>1){
+        size_t i;
+        for (i=0; i< nb -1; i++){
+            size_t j = i +rand() / (RAND_MAX/(nb-i)+1);
+            int t = sortedList[j];
+            sortedList[j]=sortedList[i];
+            sortedList[i]=t;
+        }
+    }
+    return sortedList;
+};
+
+int * sortValue(instance *inst){
+    int nb = inst->nbObjet;
+    int * sortedList = malloc (sizeof(int)*nb);
+    for( int i=0;i<nb;i++){
+        sortedList[i]=i;
+    }
+    if (nb>1){
+        size_t i;
+        for (i=0; i< nb; i++){
+            for (int j =0; j< nb ;j++){
+                if (inst->valeurObj[i] < inst->valeurObj[j]){
+                    int t = sortedList[j];
+                    sortedList[j]=sortedList[i];
+                    sortedList[i]=t;
+                }
+            }
+        }
+    }
+    return sortedList;
+};
+
+int * sortRatioValueSum(instance *inst){
+    int nb = inst->nbObjet;
+    int * sortedList = malloc (sizeof(int)*nb);
+    for( int i=0;i<nb;i++){
+        sortedList[i]=i;
+    }
+    if (nb>1){
+        size_t i;
+        for (i=0; i< nb; i++){
+            int dimI =0;
+            for (int dim=0;dim<inst->nbDim;dim++){
+                dimI+= inst->poidObj[i][dim];
+            }
+            for (int j =0; j< nb ;j++){
+                int dimJ=0;
+                for (int dim=0;dim<inst->nbDim;dim++){
+                    dimJ+=inst->poidObj[j][dim];
+                }
+                if ((inst->valeurObj[i]/dimI) < (inst->valeurObj[j]/dimJ)){
+                    int t = sortedList[j];
+                    sortedList[j]=sortedList[i];
+                    sortedList[i]=t;
+                }
+            }
+        }
+    }
+    return sortedList;
+};
+
+int * sortRatioValueDimCrit(instance *inst){
+    int nb = inst->nbObjet;
+    int dimCrit=inst->capaciteSac[0];
+    int * sortedList = malloc (sizeof(int)*nb);
+    for( int i=0;i<nb;i++){
+        sortedList[i]=i;
+    }
+    if (nb>1){
+        size_t i;
+        for (i=0; i< nb; i++){
+            for (int dim=1; dim< inst->nbDim; dim++){
+                if (inst->capaciteSac[dim]< dimCrit){
+                    dimCrit = inst->capaciteSac[dim];
+                }
+            }
+            for (int j =0; j< nb ;j++){
+                if ((inst->valeurObj[i]/dimCrit) < (inst->valeurObj[j]/dimCrit)){
+                    int t = sortedList[j];
+                    sortedList[j]=sortedList[i];
+                    sortedList[i]=t;
+                }
+            }
+        }
+    }
+    return sortedList;
+};
+
 solution * heuristique(instance * inst, functionSort fun, int typeCodage)
 {
     int l = 0;
--- heuristiques.h
+++ heuristiques.h
@@ -2,7 +2,9 @@
 
 
 typedef int * (*functionSort)(instance * inst);
-void sortRandom(instance * inst);
+int * sortRandom(instance * inst);
+int * sortValue(instance *inst);
+int * sortRatioValueSum(instance *inst);
 solution * heuristique(instance * inst, functionSort fun, int typeCodage);
 int isItemFitting(int * item, int * roomBag, int nbDim);
 void addItemToBag(int * item, int * roomBag, int nbDim);
