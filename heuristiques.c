4d4942078bea9c70c7111ef1b4e8ef91ca4ed783
MaestracciDamien <damien.maestracci@etu.univ-nantes.fr>
2017-01-26 20:40:00 +0100

    fix sorts

--- heuristiques.c
+++ heuristiques.c
@@ -50,12 +50,12 @@ int * sortRatioValueSum(instance *inst){
         for (i=0; i< nb; i++){
             int dimI =0;
             for (int dim=0;dim<inst->nbDim;dim++){
-                dimI+= inst->poidObj[i][dim];
+                dimI+= inst->poidObj[dim][i];
             }
             for (int j =0; j< nb ;j++){
                 int dimJ=0;
                 for (int dim=0;dim<inst->nbDim;dim++){
-                    dimJ+=inst->poidObj[j][dim];
+                    dimJ+=inst->poidObj[dim][j];
                 }
                 if ((inst->valeurObj[i]/dimI) < (inst->valeurObj[j]/dimJ)){
                     int t = sortedList[j];
@@ -71,6 +71,11 @@ int * sortRatioValueSum(instance *inst){
 int * sortRatioValueDimCrit(instance *inst){
     int nb = inst->nbObjet;
     int dimCrit=inst->capaciteSac[0];
+    for (int dim=1; dim< inst->nbDim; dim++){
+        if (inst->capaciteSac[dim]< dimCrit){
+            dimCrit = inst->capaciteSac[dim];
+        }
+    }
     int * sortedList = malloc (sizeof(int)*nb);
     for( int i=0;i<nb;i++){
         sortedList[i]=i;
@@ -78,11 +83,6 @@ int * sortRatioValueDimCrit(instance *inst){
     if (nb>1){
         size_t i;
         for (i=0; i< nb; i++){
-            for (int dim=1; dim< inst->nbDim; dim++){
-                if (inst->capaciteSac[dim]< dimCrit){
-                    dimCrit = inst->capaciteSac[dim];
-                }
-            }
             for (int j =0; j< nb ;j++){
                 if ((inst->valeurObj[i]/dimCrit) < (inst->valeurObj[j]/dimCrit)){
                     int t = sortedList[j];
@@ -100,17 +100,13 @@ solution * heuristique(instance * inst, functionSort fun, int typeCodage)
     int l = 0;
     int j;
     int * poidObjet;
-     printf("%d , ", inst->nbObjet);
     int nb = inst->nbObjet;
     int nbD = inst->nbDim;
-
-     puts("bonjour");
     int * sac= (int *) malloc (sizeof(int)* nb);
     for(int p =0; p< nbD; p ++)
         {
             sac[p] = inst->capaciteSac[p];
         }
-         puts("bonjour");
     solution * retour = (solution *) malloc (sizeof(solution));
     initSolution(retour,inst, typeCodage);
     int * tab = (*fun)(inst);
--- heuristiques.h
+++ heuristiques.h
@@ -5,6 +5,7 @@ typedef int * (*functionSort)(instance * inst);
 int * sortRandom(instance * inst);
 int * sortValue(instance *inst);
 int * sortRatioValueSum(instance *inst);
+int * sortRatioValueDimCrit(instance *inst);
 solution * heuristique(instance * inst, functionSort fun, int typeCodage);
 int isItemFitting(int * item, int * roomBag, int nbDim);
 void addItemToBag(int * item, int * roomBag, int nbDim);
